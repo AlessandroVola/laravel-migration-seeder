ced176d474920269e14149edf042f4b1e1fe7fe4
AlessandroVola <volaalessandro@gmail.com>
2022-11-03 19:06:36 +0100

    try 1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trains</title>
+</head>
+<body>
+    <div>
+        @foreach($trains as $train)
+            <div>
+                <h3>{{ $train['azienda'] }}</h3>
+                <p>{{ $train['stazione_partenza'] }}</p>
+                <p>{{ $train['stazione_arrivo'] }}</p>
+                <p>{{ $train['orario_partenza'] }}</p>
+            </div>
+
+        @endforeach
+    </div>
+</body>
+</html>
